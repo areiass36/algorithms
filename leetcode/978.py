92ee4b4c53b22c9ac79bdaabe55f0431d20bc6cd
Augusto Areias <areiass36@gmail.com>
2024-04-02 20:24:01 -0300

    Xor 978

--- leetcode/978.py
+++ leetcode/978.py
@@ -8,25 +8,46 @@ class Solution:
         curLower = 1
         maxLower = 1
         for i in range(1, len(arr)):
-            if i % 2 == 0 and arr[i] > arr[i-1]:
-                curGreater += 1
-            elif i % 2 == 1 and arr[i] < arr[i-1]:
-                curGreater += 1
-            else:
-                curGreater = 1
-            maxGreater = max(curGreater, maxGreater)
-
-        for i in range(1, len(arr)):
-            if i % 2 == 0 and arr[i] < arr[i-1]:
-                curLower += 1
-            elif i % 2 == 1 and arr[i] > arr[i-1]:
-                curLower += 1
+            if arr[i] > arr[i - 1]:
+                if i % 2 == 0:
+                    curGreater += 1
+                    curLower = 1
+                else:
+                    curLower += 1
+                    curGreater = 1
+            elif arr[i] < arr[i-1]:
+                if i % 2 == 0:
+                    curLower += 1
+                    curGreater = 1
+                else:
+                    curLower = 1
+                    curGreater += 1
             else:
                 curLower = 1
+                curGreater = 1
+
+            maxGreater = max(curGreater, maxGreater)
             maxLower = max(curLower, maxLower)
 
         return max(maxGreater, maxLower)
 
+    def maxTurbulenceSize2(self, arr: List[int]) -> int:
+        if len(arr) == 1:
+            return 1
+        flag = arr[0] > arr[1]
+        curSum = 2 if arr[0] != arr[1] else 1
+        maxSum = curSum
+
+        for i in range(2, len(arr)):
+            curFlag = arr[i-1] > arr[i]
+            if flag ^ curFlag and arr[i] != arr[i-1]:
+                curSum += 1
+            else:
+                curSum = 2 if arr[i] != arr[i-1] else 1
+            maxSum = max(curSum, maxSum)
+            flag = curFlag
+        return maxSum
+
 
 number = Solution()
-print(number.maxTurbulenceSize([9, 4, 2, 10, 7, 8, 8, 1, 9]))
+print(number.maxTurbulenceSize2())
